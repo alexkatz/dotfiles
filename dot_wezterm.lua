c132cdd22ce69230cd33dcdbead55ba07dac2aa3
Alex Katz <4224538+alexkatz@users.noreply.github.com>
2024-07-17 13:19:28 -0400

    colorscheme and wezterm window load functionality

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,6 +2,14 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+wezterm.on("gui-startup", function(cmd)
+	local active_screen = wezterm.gui.screens().active
+	local _, _, window = wezterm.mux.spawn_window(cmd or {})
+	local SIZE = 0.9
+	window:gui_window():set_inner_size(active_screen.width * SIZE, active_screen.height * SIZE)
+	window:gui_window():set_position(active_screen.width * (1 - SIZE) / 2, active_screen.height * (1 - SIZE) / 2)
+end)
+
 config.audible_bell = "Disabled"
 
 config.color_scheme = "Catppuccin Macchiato"
@@ -11,10 +19,12 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.80
 config.macos_window_background_blur = 90
+config.adjust_window_size_when_changing_font_size = false
 
-config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })
+config.font = wezterm.font("FiraCode Nerd Font", { weight = "DemiBold" })
+config.font_size = 14.0
 
 config.underline_position = -5
 config.underline_thickness = 1
