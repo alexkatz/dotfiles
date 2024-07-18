0089b68ee2bc2894d7ceeb51ca2a2c4a56cab9ad
Alex Katz <4224538+alexkatz@users.noreply.github.com>
2024-07-18 14:37:04 -0400

    wezterm window management, neovim config updates, use nvim as git difftool

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,13 +1,25 @@
 local wezterm = require("wezterm")
 
+local RATIO = 0.92
+
 local config = wezterm.config_builder()
+
+local position_window = function(opts)
+	opts.window:set_inner_size(
+		opts.active_screen.width * opts.screen_ratio,
+		opts.active_screen.height * opts.screen_ratio
+	)
+
+	opts.window:set_position(
+		opts.active_screen.width * (1 - opts.screen_ratio) / 2,
+		opts.active_screen.height * (1 - opts.screen_ratio) / 2
+	)
+end
 
 wezterm.on("gui-startup", function(cmd)
 	local active_screen = wezterm.gui.screens().active
 	local _, _, window = wezterm.mux.spawn_window(cmd or {})
-	local SIZE = 0.9
-	window:gui_window():set_inner_size(active_screen.width * SIZE, active_screen.height * SIZE)
-	window:gui_window():set_position(active_screen.width * (1 - SIZE) / 2, active_screen.height * (1 - SIZE) / 2)
+	position_window({ active_screen = active_screen, screen_ratio = RATIO, window = window:gui_window() })
 end)
 
 config.audible_bell = "Disabled"
@@ -34,6 +46,21 @@ config.window_padding = {
 	right = 0,
 	top = 0,
 	bottom = 0,
+}
+
+config.keys = {
+	{
+		key = "f",
+		mods = "CMD|SHIFT",
+		action = wezterm.action_callback(function(win)
+			local active_screen = wezterm.gui.screens().active
+			if win:get_dimensions().is_full_screen then
+				win:toggle_fullscreen()
+			end
+
+			position_window({ active_screen = active_screen, screen_ratio = RATIO, window = win })
+		end),
+	},
 }
 
 return config
