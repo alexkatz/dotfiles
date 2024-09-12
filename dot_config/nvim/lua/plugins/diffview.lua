d65424ea28b4ab42c18cff832456cf904a944252
Alex Katz <4224538+alexkatz@users.noreply.github.com>
2024-09-12 15:48:56 -0400

    uncomment diffview git branch compare stuff

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -1,21 +1,20 @@
 local open_diff_view = function()
-  require('diffview').open({ 'development' })
-  -- require('fzf-lua').git_branches({
-  --   actions = {
-  --     ['default'] = function(selected)
-  --       local selection = selected[1]
-  --
-  --       local current_branch_char = string.find(selection, '*')
-  --       if current_branch_char ~= nil then
-  --         selection = string.sub(selection, 3)
-  --       end
-  --
-  --       selection = vim.trim(selection)
-  --
-  --       require('diffview').open({ selection })
-  --     end,
-  --   },
-  -- })
+  require('fzf-lua').git_branches({
+    actions = {
+      ['default'] = function(selected)
+        local selection = selected[1]
+
+        local current_branch_char = string.find(selection, '*')
+        if current_branch_char ~= nil then
+          selection = string.sub(selection, 3)
+        end
+
+        selection = vim.trim(selection)
+
+        require('diffview').open({ selection })
+      end,
+    },
+  })
 end
 
 return {
