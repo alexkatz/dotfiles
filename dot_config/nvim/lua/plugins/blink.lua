a8c32c1c38df5653e32cb877dc9df087d4299fe6
Alex Katz <4224538+alexkatz@users.noreply.github.com>
2024-11-07 11:42:27 -0500

    added blink config

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -6,6 +6,10 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
     -- your own keymap.
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'default',
+      ['<C-k>'] = { 'select_prev', 'fallback' },
+      ['<C-j>'] = { 'select_next', 'fallback' },
+    },
   },
 }
