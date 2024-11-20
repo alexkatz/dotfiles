2dcbe09171c23e9a97323f700a70995da84516bc
Alex Katz <4224538+alexkatz@users.noreply.github.com>
2024-11-20 15:50:18 -0500

    updated lazyvim, wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -216,6 +216,22 @@ config.keys = {
 		action = wezterm.action({ ActivateTabRelative = -1 }),
 	},
 
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+
 	-- copy mode
 	{
 		key = "Escape",
