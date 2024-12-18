698067d5b2143754ce319a5d510910f30a6b0bac
Alex Katz <4224538+alexkatz@users.noreply.github.com>
2024-12-18 16:08:22 -0500

    noice lsp hover silent

---
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,10 @@
+return {
+  'folke/noice.nvim',
+  opts = {
+    lsp = {
+      hover = {
+        silent = true, -- set to true to not show a message if hover is not available
+      },
+    },
+  },
+}
