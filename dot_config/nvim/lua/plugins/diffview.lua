b8ae8d4ab75a3e81c2e67a52f80ca5ddf1baa786
Alex Katz <4224538+alexkatz@users.noreply.github.com>
2025-02-04 12:40:34 -0500

    removed fzf-lua

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -1,28 +1,19 @@
-local open_diff_view = function()
-  require('fzf-lua').git_branches({
-    actions = {
-      ['enter'] = function(selected)
-        local selection = selected[1]
-
-        local current_branch_char = string.find(selection, '*')
-        if current_branch_char ~= nil then
-          selection = string.sub(selection, 3)
-        end
-
-        selection = vim.trim(selection)
-
-        require('diffview').open({ selection })
-      end,
-    },
-  })
-end
-
 return {
   'sindrets/diffview.nvim',
   dependencies = 'nvim-lua/plenary.nvim',
   keys = {
     { '<leader>gg', '<cmd>DiffviewOpen<cr>', desc = 'Diff workspace changes' },
-    { '<leader>gG', open_diff_view, desc = 'Diff with selected branch' },
+    {
+      '<leader>gG',
+      function()
+        Snacks.picker.git_branches({
+          confirm = function(_, item)
+            require('diffview').open({ item.branch })
+          end,
+        })
+      end,
+      desc = 'Diff with selected branch',
+    },
     { '<leader>gF', '<cmd>DiffviewFileHistory %<cr>', desc = 'File history (this file)' },
     { '<leader>gf', '<cmd>DiffviewFileHistory<cr>', desc = 'File history' },
   },
