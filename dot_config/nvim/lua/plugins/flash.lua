f4fd2893d4ad36c16e27e31ee70cdfeb513844e2
Alex Katz <4224538+alexkatz@users.noreply.github.com>
2025-09-17 22:16:21 -0400

    treesitter related changes

--- dot_config/nvim/lua/plugins/flash.lua
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -1,13 +1,40 @@
 return {
   'folke/flash.nvim',
   opts = {
-    label = {
-      rainbow = {
-        enabled = true,
+    modes = {
+      treesitter = {
+        search = { incremental = true },
+        label = { after = false, before = false },
+      },
+      treesitter_search = {
+        jump = { pos = 'range' },
+        search = { multi_window = true, wrap = true, incremental = false },
+        remote_op = { restore = true },
+        label = { before = true, after = true, style = 'inline' },
       },
     },
-    modes = {
-      search = { enabled = true },
+  },
+
+  keys = {
+    {
+      '<CR>',
+      function()
+        require('flash').treesitter({
+          actions = {
+            ['<CR>'] = 'next',
+            ['<BS>'] = 'prev',
+          },
+        })
+      end,
+      { desc = 'Treesitter incremental selection' },
+    },
+
+    {
+      'S',
+      function()
+        require('flash').treesitter_search()
+      end,
+      { desc = 'Treesitter search' },
     },
   },
 }
