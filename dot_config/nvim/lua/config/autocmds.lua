36c15bb8e09a0a144131249536f1fbccbac1c877
Alex Katz <4224538+alexkatz@users.noreply.github.com>
2026-03-02 11:53:21 -0500

    nvim 0.11 LspEslintFixAll fix

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -2,3 +2,16 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
+
+-- ESLint fix-all on save (Nvim 0.11+ uses LspEslintFixAll instead of EslintFixAll)
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client.name == 'eslint' then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = args.buf,
+        command = 'LspEslintFixAll',
+      })
+    end
+  end,
+})
