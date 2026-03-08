90ee0b421cc9642166b05b4df4d3529f991fe477
Alex Katz <4224538+alexkatz@users.noreply.github.com>
2026-03-08 14:28:57 -0400

    replace diffview with codediff

--- dot_config/nvim/lua/plugins/codediff.lua
+++ dot_config/nvim/lua/plugins/codediff.lua
@@ -2,11 +2,42 @@ return {
   'esmuellert/codediff.nvim',
   dependencies = { 'MunifTanjim/nui.nvim' },
   cmd = 'CodeDiff',
+  opts = {
+    keymaps = {
+      view = {
+        next_file = '<tab>',
+        prev_file = '<s-tab>',
+        close_on_open_in_prev_tab = true,
+      },
+    },
+  },
   keys = {
     {
-      '<leader>gv',
-      function() end,
-      desc = 'Code Diff',
+      '<leader>gg',
+      '<cmd>CodeDiff<cr>',
+      desc = 'Diff workspace changes',
+    },
+    {
+      '<leader>gG',
+      function()
+        Snacks.picker.git_branches({
+          all = true,
+          confirm = function(_, item)
+            vim.cmd('CodeDiff ' .. item.branch)
+          end,
+        })
+      end,
+      desc = 'Diff with selected branch',
+    },
+    {
+      '<leader>gf',
+      '<cmd>CodeDiff history %<cr>',
+      desc = 'File history (this file)',
+    },
+    {
+      '<leader>gF',
+      '<cmd>CodeDiff history<cr>',
+      desc = 'File history',
     },
   },
 }
